eeb9701b31f7902104eed42cfbe151bb8b16c00d
ladyaev.alexander <alex.ladyaev2013@yandex.ru>
2023-06-12 13:13:27 +0400

    add vibration

--- app/src/main/java/ldev/myNotifier/presentation/fragments/editOneTimeNotification/EditOneTimeNotificationFragment.kt
+++ app/src/main/java/ldev/myNotifier/presentation/fragments/editOneTimeNotification/EditOneTimeNotificationFragment.kt
@@ -1,10 +1,19 @@
 package ldev.myNotifier.presentation.fragments.editOneTimeNotification
 
+import android.content.Context
+import android.content.Context.VIBRATOR_SERVICE
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.os.VibratorManager
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.NumberPicker
 import ldev.myNotifier.R
 import ldev.myNotifier.databinding.FragmentEditOneTimeNotificationBinding
 import ldev.myNotifier.utils.BaseFragment
@@ -17,9 +26,32 @@ class EditOneTimeNotificationFragment : BaseFragment<FragmentEditOneTimeNotifica
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.intervalPicker.minValue = 5
-        binding.intervalPicker.maxValue = 24
-        binding.intervalPicker.wrapSelectorWheel = false
+
+        val vibrator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val vibratorManager = requireActivity().getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+            vibratorManager.defaultVibrator
+        } else {
+            @Suppress("DEPRECATION")
+            requireActivity().getSystemService(VIBRATOR_SERVICE) as Vibrator
+        }
+
+        val handler = Handler(Looper.getMainLooper())
+        val vibrationRunnable = Runnable {
+            val effect = VibrationEffect.createOneShot(150, 1)
+            vibrator.vibrate(effect)
+        }
+
+        binding.hourPicker.minValue = 0
+        binding.hourPicker.maxValue = 23
+        binding.hourPicker.wrapSelectorWheel = false
+
+        binding.hourPicker.setOnValueChangedListener { _, _, _ ->
+            handler.post(vibrationRunnable)
+        }
+
+        binding.minutePicker.minValue = 1
+        binding.minutePicker.maxValue = 59
+        binding.minutePicker.wrapSelectorWheel = false
     }
 
 }
